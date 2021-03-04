f4946d6a32dfe4e3fc7663c355113c9b2a603a9d
kajal1sharma <kajal1231995@gmail.com>
2021-03-04 13:44:36 +0530

    print program

---
+++ printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+
+int main()
+{
+    printf("%f  \n",3.4);
+    printf("the number\n is %f  this\n",3.4);
+    printf("this is also a message");
+    printf("%d  ::",12);
+    return 0;
+}
