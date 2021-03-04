8a7855df5dc981ac6de68704f4150b38515bd0a7
kajal1sharma <kajal1231995@gmail.com>
2021-03-04 13:53:55 +0530

    comments

---
+++ comments.c
@@ -0,0 +1,21 @@
+      //this is my program for printing hello world
+    //printf("hello world");
+#include <stdio.h>
+    //this is my program for printing hello world
+    //printf("hello world");
+
+int main()
+{
+    /* this is 
+    multi 
+    line comment
+    */
+    printf("multi line comment");
+    //this is a comment
+    //this is my program for printing hello world
+    //printf("hello world");
+    printf("hello world");
+    return 0;
+}
+    //this is my program for printing hello world
+    //printf("hello world");
