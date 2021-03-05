fb212f64b4fcc23eab9978e32a692422d47bb34f
kajal1sharma <kajal1231995@gmail.com>
2021-03-05 13:56:27 +0530

    cpp prog

---
+++ datatype.cpp
@@ -0,0 +1,17 @@
+
+#include <iostream>
+
+using namespace std;
+int main()
+{
+    //storage basics
+    //data type=> int , float, char
+    int a=50;
+    float b=45.67;
+    char c='a';
+    cout<<a<<"\n";
+    cout<<b<<"\n";
+    cout<<"this\tis\tmy\tmessage";
+    
+    return 0;
+}
