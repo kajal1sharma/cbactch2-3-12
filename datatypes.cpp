b888b3ce1709dd28e17ae6234583dce2dba626ad
kajal1sharma <kajal1231995@gmail.com>
2021-03-14 13:47:44 +0530

    14/3/21

---
+++ datatypes.cpp
@@ -0,0 +1,31 @@
+//%
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    // int a=100;
+    // char b='a';
+    // float c=90.78;
+    // short d=89;
+    // long e=67;
+    // double f=89.7;
+    
+   // cout<<sizeof(f);
+
+unsigned int a=2147483648;
+
+cout<<a;
+
+
+
+
+
+
+
+
+
+
+    return 0;
+}
---
+++ if_else.cpp
@@ -0,0 +1,17 @@
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int laptop;
+    cin>>laptop;
+    if(1){//laptop<50000
+        cout<<"we will buy this one";
+    }
+    else{
+        cout<<"we will not buy this one";
+    }
+    return 0;
+}
