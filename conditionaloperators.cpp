7d4ae7a4b20eca95eddc073a02aa7a0c7596de7b
kajal1sharma <kajal1231995@gmail.com>
2021-03-31 13:56:08 +0530

    31/03

---
+++ conditionaloperators.cpp
@@ -0,0 +1,49 @@
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+//> < == != >= <=
+
+//binary operators
+// two operands 
+//+ - / *
+//these alll are operators ->binary 
+//plus =>two variables two values 
+//2*4 multiplication operator =>values  
+
+//+ operator two variables/operands and addition result
+
+//> two operands =>result true/FALSE
+// a<b,a>b,a>=b,a<=b,a==b,a!=b =>a and b are my variables/operands and operator true or FALSE
+
+
+int a ;
+int b;
+cin>>a;
+cin>>b;
+
+//int=>storing numbers ,bool =>datatype is used for storing true and FALSE
+
+// bool result=a<b;
+// //true=>1//false=>0
+// cout<<result;
+
+// bool result =a>b;//12>23
+// cout<<result;
+
+// bool result = a<=b;   //12 <= 12  12<=15    15<=12
+// cout<<result;
+
+// bool result = a>=b;
+// cout<<result;
+
+// bool result =  a == b;
+// cout<<result;
+
+// bool result =  a != b;//a=12  b=13   result=true
+// cout<<result;
+    return 0;
+}
