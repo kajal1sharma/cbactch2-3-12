e2739b25bbc03cb435f02cabc7daa8ca0c590471
kajal1sharma <kajal1231995@gmail.com>
2021-04-13 14:01:31 +0530

    13/04

---
+++ pattern.c
@@ -0,0 +1,93 @@
+#include <stdio.h>
+// *
+// **
+// ***
+// ****
+// *****
+
+// *****
+// ****
+// ***
+// **
+// *
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    
+    for(int i=1;i<=n;i++){
+         
+         for(int j=1;j<=i;j++){
+            printf("*");
+        }
+        printf("\n");
+        
+    }
+    
+    
+    
+    // for(int i=1;i<=1;i++){
+    //     printf("*");
+    // }
+    // printf("\n");
+    // for(int i=1;i<=2;i++){
+    //     printf("*");
+    // }
+    // printf("\n");
+    // for(int i=1;i<=3;i++){
+    //     printf("*");
+    // }
+    // printf("\n");
+    // for(int i=1;i<=4;i++){
+    //     printf("*");
+    // }
+    // printf("\n");
+    // for(int i=1;i<=5;i++){
+    //     printf("*");
+    // }
+    // printf("\n");
+    
+    
+    
+//   // printf("*");
+//     for(int i=1;i<=1;i++){
+//         printf("*");
+//     }
+//     printf("\n");
+    
+//     // printf("*");
+//     // printf("*");
+//     for(int i=1;i<=2;i++){
+//         printf("*");
+//     }
+//     printf("\n");
+    
+//     // printf("*");
+//     // printf("*");
+//     // printf("*");
+//     for(int i=1;i<=3;i++){
+//         printf("*");
+//     }
+//     printf("\n");
+    
+//     // printf("*");
+//     // printf("*");
+//     // printf("*");
+//     // printf("*");
+//     for(int i=1;i<=4;i++){
+//         printf("*");
+//     }
+//     printf("\n");
+    
+    
+//     // printf("*");
+//     // printf("*");
+//     // printf("*");
+//     // printf("*");
+//     // printf("*");
+//     for(int i=1;i<=5;i++){
+//         printf("*");
+//     }
+//     printf("\n");
+    return 0;
+}
