58e4e7f457b2704856871b955197ac4d30d643b8
kajal1sharma <kajal1231995@gmail.com>
2021-04-20 13:36:14 +0530

    20/4

---
+++ addfunc.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+//return 
+//type    //name of function    
+void      sum(){
+    printf("enter two number");
+    int a;
+    int b;
+    scanf("%d%d",a,b);
+    int sum=a+b;
+    printf("%d",sum);
+}
+
+int main()
+{
+   
+  sum();//calling a function
+    return 0;
+}
---
+++ addfunc.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+//return 
+//type    //name of function    
+void      sum(){
+    cout<<"enter two number";
+    int a;
+    int b;
+    cin>>a>>b;
+    int sum=a+b;
+    cout<<sum;
+}
+
+int main()
+{
+   
+  sum();//calling a function
+    return 0;
+}
