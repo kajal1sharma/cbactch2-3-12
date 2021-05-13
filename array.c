6ac9a33aeab2b83282afb9ffbdfe42e487601d0c
kajal1sharma <kajal1231995@gmail.com>
2021-05-13 14:21:44 +0530

    13/5

---
+++ array.c
@@ -0,0 +1,29 @@
+
+#include <stdio.h>
+
+int main()
+{
+    int aa=90;
+    int b=89;
+    int c=23;
+    int d=34;
+    int e=56;
+    scanf("%d",&c);
+    printf("%d",c);
+    
+   // type_of _array name_of_array[size of array]   
+   // char,int,float  myarr
+    char myarr[20];
+    myarr[0]='a';
+    myarr[1]='b';
+    
+    int a[5];
+    a[0]=12;
+    a[1]=234;
+    a[2]=33;
+    a[3]=45;
+    a[4]=543;
+       scanf("%d",&a[2]);
+     printf("%d",a[2]);
+    return 0;
+}
