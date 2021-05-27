1c23efdeb406c7666a4bd6915898f584aa8d4778
kajal1sharma <kajal1231995@gmail.com>
2021-05-27 14:24:41 +0530

    27/5

--- structure1.c
+++ structure1.c
@@ -12,10 +12,11 @@ int main()
 {
     int a=90;
     //data type
-    stuct Student  stu1;
+    struct Student  stu1;
     stu1.rollno=78;
     stu1.marks=78.98;
     stu1.grade='A';
-    
+    printf("%c",stu1.grade);
+    printf("%f", stu1.marks);
     return 0;
 }
