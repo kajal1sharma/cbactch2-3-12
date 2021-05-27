754d9d163acd1a84f19dd94bcd7700f66a1a5fe9
kajal1sharma <kajal1231995@gmail.com>
2021-05-27 15:15:20 +0530

    27/5

---
+++ exception1.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    //system generated exception
+    int a=90/0;
+    
+    // try{
+    //     int num=0;
+    //     cin>>num;
+    //     if(num==45){
+    //         throw(num);
+    //     }
+    //     else{
+    //         num=num+10;
+    //         cout<<num;
+    //     }
+    // }
+    // catch(int e){
+    //     cout<<"it is an exception";
+    // }
+
+    return 0;
+}
--- structure1.c
+++ structure1.c
@@ -20,3 +20,27 @@ int main()
     printf("%f", stu1.marks);
     return 0;
 }
+/*
+
+
+struct Student{
+    int rollno;
+    float marks;
+    char grade;
+};
+
+
+#include <stdio.h>
+
+int main()
+{
+    int a=90;
+    //data type
+    //initialization
+    struct Student  stu1={12,45.5,'F'};
+    struct Student  stu1={.grade='h',.marks=78.9,.rollno=89};
+    scanf("%d%f %c",&stu1.rollno,&stu1.marks,&stu1.grade);
+    printf("%d  %f   %c",stu1.rollno,stu1.marks,stu1.grade);
+    return 0;
+}
+*/
